fa229e20bf9cf6f9145bf1f3f9f9def8ed22d986
Parama Hamsa Vadlamudi <paramahamsa326@users.noreply.github.com>
2017-02-14 12:25:39 -0800

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,7 @@
 public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello World \nHello Java");
+		
+		System.out.println("Hello World \n Now Hello Java");
 	}
 }
