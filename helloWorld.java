499fa5bf07b549c820c83713a8cf0da02ef6368c
Parama Hamsa Vadlamudi <paramahamsa326@users.noreply.github.com>
2017-02-15 11:06:09 -0800

    Update helloWorld.java
    Next day commit to second build

--- helloWorld.java
+++ helloWorld.java
@@ -8,5 +8,7 @@ public class HelloJava {
 		System.out.println("Hello World \n Now from Jenkins Hello Java");
 		
 		System.out.println("Next day commit to build");
+		
+		System.out.println("Next day commit to second build");
 	}
 }
