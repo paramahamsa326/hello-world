524fcaffb41a5211952cd2400a455269b2086cc6
Parama Hamsa Vadlamudi <paramahamsa326@users.noreply.github.com>
2017-02-21 16:14:22 -0800

    commited

--- helloWorld.java
+++ helloWorld.java
@@ -1,14 +1,6 @@
 
 public class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("Hello World \nHello Java");
-		
-		System.out.println("Hello World \n Now Hello Java");
-		
-		System.out.println("Hello World \n Now from Jenkins Hello Java");
-		
-		System.out.println("Next day commit to build");
-		
-		System.out.println("Next day commit to second build");
+		System.out.println("Hello World Pro-Tek Consulting Java");
 	}
 }
